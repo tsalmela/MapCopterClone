d7a5e93214bad6976ec52cd469b60f7adf591581
Joona Hartikainen <syp@tux.fi>
2016-02-29 10:42:31 +0200

    post all events on main thread

--- app/src/main/java/fi/oulu/mapcopter/MapCopterApplication.java
+++ app/src/main/java/fi/oulu/mapcopter/MapCopterApplication.java
@@ -1,15 +1,18 @@
 package fi.oulu.mapcopter;
 
 import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.squareup.otto.Bus;
 
 import fi.oulu.mapcopter.copter.CopterManager;
+import fi.oulu.mapcopter.event.MainThreadEventBus;
 
 public class MapCopterApplication extends Application {
 
     private static CopterManager copterManager;
-    private static Bus defaultBus = new Bus();
+    private static Bus defaultBus = new MainThreadEventBus(new Handler(Looper.getMainLooper()));
 
     public static CopterManager getCopterManager() {
         return copterManager;
