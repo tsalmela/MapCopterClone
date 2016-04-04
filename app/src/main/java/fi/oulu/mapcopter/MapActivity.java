565942e15d71feaca760855e094de061b5b849b6
syp <syp@tux.fi>
2016-04-05 01:02:17 +0300

    fixed broken build after merge

--- app/src/main/java/fi/oulu/mapcopter/MapActivity.java
+++ app/src/main/java/fi/oulu/mapcopter/MapActivity.java
@@ -1,15 +1,11 @@
 package fi.oulu.mapcopter;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.TextureView;
-import android.view.View;
-import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -31,7 +27,6 @@ import java.util.List;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import fi.oulu.mapcopter.copter.AircraftPositionChangeListener;
-import fi.oulu.mapcopter.copter.CameraManager;
 import fi.oulu.mapcopter.copter.CopterManager;
 import fi.oulu.mapcopter.event.CopterConnectionEvent;
 import fi.oulu.mapcopter.event.CopterStatusChangeEvent;
@@ -43,35 +38,6 @@ public class MapActivity extends AppCompatActivity implements AircraftPositionCh
     public static final double MAP_BOUNDS_LIMIT_LATITUDE = 0.0035;
     public static final double MAP_BOUNDS_LIMIT_LONGITUDE = 0.01;
 
-    private static final int UI_ANIMATION_DELAY = 500;
-    private static final String FLAG_CONNECTION_CHANGE = "fi_oulu_mapcopter_connection_change";
-    private final Handler mHideHandler = new Handler();
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    //private View mContentView;
-    private Handler mHandler;
-    private Runnable updateRunnable = new Runnable() {
-        @Override
-        public void run() {
-            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
-            Log.d(TAG, "update runnable");
-            sendBroadcast(intent);
-        }
-    };
-    private TextureView mCameraView;
-    private DJICamera mCamera;
-    private MapCopterManager mapCopterManager;
-    private VideoSurfaceListener surfaceListener;
-    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            onProductChange();
-        }
-    };
     private CopterManager mapCopterManager;
     private GoogleMap mMap;
     private Marker aircraftLocationMarker;
@@ -79,45 +45,38 @@ public class MapActivity extends AppCompatActivity implements AircraftPositionCh
 
     private Bus eventBus;
     private Polygon boundsPolygon;
-    private View mContentView;
-    private Button mStopButton;
-    private SeekBar seekBar;
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         ButterKnife.bind(this);
-        initializeVariables();
 
-        // Initialize the textview with '0'.
-        textView.setText("Korkeus: " + seekBar.getProgress() + "/" + seekBar.getMax());
+        prepareMap();
+
+        final TextView heightText = (TextView) findViewById(R.id.textView);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        heightText.setText("Korkeus: " + seekBar.getProgress() + "/" + seekBar.getMax());
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = 0;
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
                 progress = progresValue;
-                Toast.makeText(getApplicationContext(), "Muutetaan korkeutta", Toast.LENGTH_SHORT).show();
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 String progressText = ("" + progress).format("%1$-" + 3 + "s", "XXX").replaceAll(" ", "0");
-                textView.setText("Korkeus: " + progressText + "/" + seekBar.getMax());
+                heightText.setText("Korkeus: " + progressText + "/" + seekBar.getMax());
                 Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
             }
         });
-
-        seekBar.setRotation(270);
-
-        prepareMap();
 
         eventBus = MapCopterApplication.getDefaultBus();
         mapCopterManager = MapCopterApplication.getCopterManager();
@@ -125,56 +84,6 @@ public class MapActivity extends AppCompatActivity implements AircraftPositionCh
 
         TextureView mCameraView = ButterKnife.findById(this, R.id.camera_view);
         VideoSurfaceListener surfaceListener = new VideoSurfaceListener(this, mapCopterManager.getCameraManager());
-        mStopButton = (Button) findViewById(R.id.button_stop);
-
-        mStopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "STOP button pushed: ");
-            }
-        });
-
-        mapCopterManager = MapCopterManager.createManager(this, this);
-        mapCopterManager.initManager();
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(FLAG_CONNECTION_CHANGE);
-
-        registerReceiver(mReceiver, filter);
-
-    }
-
-    public static String LPad(String str, Integer length, char car) {
-        return str
-                +
-                String.format("%" + (length - str.length()) + "s", "")
-                        .replace(" ", String.valueOf(car));
-    }
-
-    // A private method to help us initialize our variables.
-    private void initializeVariables() {
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
-        textView = (TextView) findViewById(R.id.textView);
-    }
-
-    private void onProductChange() {
-        if (surfaceListener != null) {
-            surfaceListener.initPreviewer();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(mReceiver);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (surfaceListener == null) {
-            surfaceListener = new VideoSurfaceListener(this, mapCopterManager);
-        }
         mCameraView.setSurfaceTextureListener(surfaceListener);
     }
 
@@ -213,34 +122,6 @@ public class MapActivity extends AppCompatActivity implements AircraftPositionCh
         LatLng centerOfMap = mMap.getCameraPosition().target;
         Log.d(TAG, centerOfMap.toString());
         moveToMapCenter();
-    @SuppressLint("InlinedApi")
-    private void hide() {
-        /*
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-                */
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        /*
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-                */
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
     private void prepareMap() {
