36a3b8d5e1fcad0a2434725934f202af73cda7a8
Topi <thoubias@gmail.com>
2016-04-17 17:13:38 +0300

    remoteConnection IP saving

--- app/src/main/java/fi/oulu/mapcopter/RemoteInfoDialogActivity.java
+++ app/src/main/java/fi/oulu/mapcopter/RemoteInfoDialogActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -12,6 +14,9 @@ import butterknife.OnClick;
 public class RemoteInfoDialogActivity extends AppCompatActivity{
 
     private static final String PREF_REMOTE_IP = "remoteIp";
+
+    @Bind(R.id.editText_remote_access_IP)
+    EditText serverAddressField;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,13 +28,25 @@ public class RemoteInfoDialogActivity extends AppCompatActivity{
         String remoteIp = PreferenceManager.getDefaultSharedPreferences(this)
                 .getString(PREF_REMOTE_IP, "");
 
+        serverAddressField.setText(remoteIp);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        String ip = serverAddressField.getText().toString();
+
         PreferenceManager.getDefaultSharedPreferences(this)
                 .edit()
-                .putString(PREF_REMOTE_IP, "1.1.1.1")
+                .putString(PREF_REMOTE_IP, ip)
                 .apply();
     }
 
-
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
 
     @OnClick(R.id.button_remoteAccess_ok)
     public void onOKClicked(){
