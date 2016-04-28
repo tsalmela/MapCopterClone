258c64b9c7850cdc661b9750cdb79b39f2980fab
Joona Hartikainen <syp@tux.fi>
2016-04-28 17:23:35 +0300

    fixed wrong aspect ratio on the camera preview

--- app/src/main/java/fi/oulu/mapcopter/copter/DJICameraManager.java
+++ app/src/main/java/fi/oulu/mapcopter/copter/DJICameraManager.java
@@ -2,13 +2,20 @@ package fi.oulu.mapcopter.copter;
 
 import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.media.MediaCodec;
+import android.media.MediaFormat;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import java.io.IOException;
+
 import dji.sdk.AirLink.DJILBAirLink;
 import dji.sdk.Camera.DJICamera;
+import dji.sdk.Camera.DJICameraSettingsDef;
 import dji.sdk.Codec.DJICodecManager;
+import dji.sdk.base.DJIBaseComponent;
 import dji.sdk.base.DJIBaseProduct;
+import dji.sdk.base.DJIError;
 
 public class DJICameraManager implements DJICamera.CameraReceivedVideoDataCallback, DJILBAirLink.DJIOnReceivedVideoCallback, CameraManager {
     private static final String TAG = DJICameraManager.class.getSimpleName();
@@ -18,6 +25,9 @@ public class DJICameraManager implements DJICamera.CameraReceivedVideoDataCallba
     public void initCamera(@NonNull DJICamera camera, @NonNull DJIBaseProduct product) {
         camera.setDJICameraReceivedVideoDataCallback(this);
         if (product.getAirLink() != null) {
+            product.getCamera().setVideoResolutionAndFrameRate(DJICameraSettingsDef.CameraVideoResolution.Resolution_1920x1080, DJICameraSettingsDef.CameraVideoFrameRate.FrameRate_30fps, null);
+            
+            product.getCamera().setPhotoRatio(DJICameraSettingsDef.CameraPhotoAspectRatio.AspectRatio_16_9, null);
             if (product.getAirLink().getLBAirLink() != null) {
                 product.getAirLink().getLBAirLink().setDJIOnReceivedVideoCallback(this);
             }
